6c8a019fb2122134fdc72c7de5ea6fd9d6042bab
Gabarros <gabarros17@gmail.com>
2019-12-18 23:47:49 -0300

    create layout and burger builder components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word!</h1>
+      <Layout>
+        <p>Layout test</p>
+      </Layout>
     </div>
   );
 }
