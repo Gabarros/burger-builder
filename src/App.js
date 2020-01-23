20a3857cc8762421c3b7b31bad678ef92896ee6c
Gabarros <gabarros17@gmail.com>
2020-01-23 12:04:21 -0300

    Add Login persistence in local storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './components/Layout';
 import BurgerBuilder from './containers/BurgerBuilder';
@@ -7,22 +8,38 @@ import Orders from './containers/Orders/Orders';
 import Checkout from './containers/Checkout/Checkout';
 import Auth from './containers/Auth';
 import Logout from './containers/Logout';
+import * as actions from './store/actions/index';
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={BurgerBuilder} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-        </Switch>
+class App extends Component {
 
-      </Layout>
-    </div>
-  );
-}
+  componentDidMount(){
+    this.props.onTryAutoSignup();
+  }
+ 
+  render(){
+    return (
+      <div className="App">
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
+          </Switch>
+  
+        </Layout>
+      </div>
+    );
+  }
 
-export default App;
+};
+
+const mapDispatchToProps = dispatch =>{
+  return{
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+};
+  
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
