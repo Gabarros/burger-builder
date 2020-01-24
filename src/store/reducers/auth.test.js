882d383012949e19a76b6bf81cfd8f63b676de72
Gabarros <gabarros17@gmail.com>
2020-01-24 12:32:49 -0300

    Add Firebase and deploy

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -15,15 +15,15 @@ describe('auth reducer', () => {
     it('should store the token after the login ', () => {
         expect(reducer({
             token: null,
-        userId: null,
-        error: null,
-        loading: false,
-        authRedirectPath: '/'
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
         }, {
             type: actionTypes.AUTH_SUCCESS,
             idToken: 'some-token',
             userId: 'some-user-id'
         }))
     });
-    
+
 });
